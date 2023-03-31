3bd9d8d063339df37376468fc712179d25aca23e
hymnstar <dlcksrb908@naver.com>
2023-03-31 16:51:09 +0900

    courier fee calc

--- volume_calc.py
+++ volume_calc.py
@@ -4,4 +4,23 @@ H = float(input("높이: "))
 
 V = W * D * H
 
-print("hello world!")
+print("total lenght: %f" %V)
+
+print("Do you want to calculate the bill? Enter Y/N")
+answer = str(input())
+
+if (answer = N):
+    print("Exit the program.")
+
+else:
+    if (V <= 80):
+        print("5$")
+
+    elif (80 < V <= 100):
+        print("8$")
+
+    elif (100 < V <= 120):
+        print("10$")
+
+    else:
+        print("It's too heavy)
